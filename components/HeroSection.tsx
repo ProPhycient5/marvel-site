47e52a8ff87a191802406b858e0ba9c65da62af9
sawan <dsawan.office8@gmail.com>
2023-12-10 19:00:56 +0530

    fixed some css

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,21 +5,21 @@ import Image from "next/image";
 export const HeroSection_2 = () => {
   return (
     <section id="hero_section">
-      <div className="my-32 pt-0 md:pt-12 px-4 w-full flex justify-between flex-col md:flex-row items-center">
-        <div className="h-56 w-10/12 md:w-48 mb-16 md:mb-0 flex flex-col ">
-          <p className="text-white font-bold text-2xl mb-2 md:mb-8 ">
+      <div className="my-32 pt-0 md:pt-12 px-0 sm:px-4 w-full flex justify-between flex-col md:flex-row items-center">
+        <div className="h-56 w-10/12 md:w-48 mb-16 md:mb-0 flex flex-col">
+          <p className="text-white font-bold text-lg sm:text-2xl lg:text-4xl mb-4 lg:mb-8 ">
             Let&#39;s build the future <br /> of the design, now.
           </p>
-          <p className="text-xs text-slate-400 text-justify md:text-left mb-2 md:mb-8">
+          <p className="text-xs text-slate-400 text-justify md:text-left mb-4 lg:mb-8">
             Building something special? We&#39;r always looking to work with
             partner who wants to help the world bring their ideas to life.
           </p>
 
-          <div className="flex flex-col md:flex-row w-full justify-center items-center md:justify-between">
-            <button className="bg-blue-400 w-48 text-xs font-semibold text-slate-100 rounded-full flex justify-center items-center py-2 px-1 md:px-3">
+          <div className="flex flex-row w-full items-center justify-between">
+            <button className="bg-blue-400 sm:w-48 text-xs font-semibold text-slate-100 rounded-full flex justify-center items-center py-2 px-2 sm:px-3">
               Submit your app
             </button>
-            <button className="bg-slate-100 text-xs w-48 font-semibold text-slate-950 rounded-full flex justify-center items-center py-2 px-3 mt-4 md:mt-0">
+            <button className="bg-slate-100 text-xs w-48 font-semibold text-slate-950 rounded-full flex justify-center items-center py-2 px-2 sm:px-3 md:mt-0">
               View API docs
             </button>
           </div>
