900d826df83438cba533d43b4bfa7d77770fed63
sawan <dsawan.office8@gmail.com>
2023-12-10 18:05:26 +0530

    added code

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,8 +6,8 @@ export const HeroSection_2 = () => {
   return (
     <section id="hero_section">
       <div className="my-32 pt-0 md:pt-12 w-full flex justify-between flex-col md:flex-row items-center">
-        <div className="h-full w-10/12 md:w-48 mb-16 md:mb-0 bg-slate-400">
-
+        <div className="h-56 w-10/12 md:w-48 mb-16 md:mb-0 bg-slate-400">
+           
         </div>
         <div className="h-60 lg:h-60 w-10/12 md:w-48">
           <Image
