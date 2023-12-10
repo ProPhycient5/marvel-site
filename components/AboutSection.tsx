4fb0feca3ef78353258753f9c1a02374c03edcf7
sawan <dsawan.office8@gmail.com>
2023-12-10 23:00:46 +0530

    added correct html tag

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -6,9 +6,9 @@ const AboutSection = () => {
   return (
     <section id="about">
       <div className="my-6 w-full flex flex-col justify-center items-center">
-        <div className="text-2xl font-bold text-center mb-6 px-2 w-full md:w-1/2">
+        <h1 className="text-2xl font-bold text-center mb-6 px-2 w-full md:w-1/2">
           Experience the freedom to build features, right away.
-        </div>
+        </h1>
         <div className="text-xs text-slate-400 text-center w-full md:w-1/2">
           You can now harness components of our platform and build powerful{" "}
           integrations for our 2 millions users - or simply just for your team.
--- components/Features.tsx
+++ components/Features.tsx
@@ -14,11 +14,11 @@ export const Architecture = () => {
             sizes="100hw"
             className="w-full h-auto sm:h-48 lg:h-full mb-2 md:mb-0"
           />
-          <div className="text-sm font-semibold mb-2">Dedicated API team</div>
-          <div className="text-xs text-slate-400 text-center">
+          <h2 className="text-sm font-semibold mb-2">Dedicated API team</h2>
+          <p className="text-xs text-slate-400 text-center">
             Our team are available for user&#39;s questions via our Slack
             Developer Community and Email
-          </div>
+          </p>
         </div>
         <div className="h-auto px-4 lg:h-60 w-10/12 base:w-32 my-5 md:my-10 flex flex-col justify-center items-center">
           <Image
@@ -29,11 +29,11 @@ export const Architecture = () => {
             sizes="100hw"
             className="w-full h-auto sm:h-48 lg:h-full mb-2 md:mb-0"
           />
-          <div className="text-sm font-semibold mb-2">Our API uses GraphQL</div>
-          <div className="text-xs text-slate-400 text-center">
+          <h2 className="text-sm font-semibold mb-2">Our API uses GraphQL</h2>
+          <p className="text-xs text-slate-400 text-center">
             No handling server side. Get many API&#39;s response in a single
             response
-          </div>
+          </p>
         </div>
         <div className="h-auto px-0 lg:px-4 lg:h-60 w-10/12 base:w-32 my-5 md:my-10 flex flex-col justify-center items-center">
           <Image
@@ -44,13 +44,13 @@ export const Architecture = () => {
             sizes="100hw"
             className="w-full h-auto sm:h-48 lg:h-full mb-2 md:mb-0"
           />
-          <div className="text-sm font-semibold mb-2">
+          <h2 className="text-sm font-semibold mb-2">
             Supercharge your workflow
-          </div>
-          <div className="text-xs text-slate-400 text-center">
+          </h2>
+          <p className="text-xs text-slate-400 text-center">
             Automate workflow, integrate and analyze data and take Marvel to the
             next level.
-          </div>
+          </p>
         </div>
       </div>
     </section>
@@ -63,14 +63,14 @@ export const TechIntegration = () => {
       <div className="my-16 w-full flex justify-between flex-col-reverse base:flex-col items-center">
         <div className="w-full px-6 h-full flex justify-between flex-col-reverse base:flex-row items-center">
           <div className="h-auto lg:h-60 w-10/12 base:w-48 my-5 md:my-10 flex flex-col">
-            <p className=" text-lg text-white font-semibold mb-8">
+            <h1 className=" text-lg text-white font-semibold mb-8">
               Showcase your app in our <br />
               integrations directory
-            </p>
-            <div className="text-xs text-slate-400 text-justify md:text-left mb-8">
+            </h1>
+            <p className="text-xs text-slate-400 text-justify md:text-left mb-8">
               Building something special? We r always looking to work with
               partner who wants to help the world bring their ideas to life.
-            </div>
+            </p>
 
             <div className="flex flex-col md:flex-row w-full justify-center items-center md:justify-between">
               <button className="bg-slate-100 w-2/3 md:w-48 text-xs font-semibold text-slate-950 rounded-full flex justify-center items-center py-2 px-1 md:px-3">
@@ -104,14 +104,14 @@ export const TechIntegration = () => {
             />
           </div>
           <div className="h-auto lg:h-60 w-10/12 base:w-48 my-5 md:my-10 flex flex-col">
-            <p className=" text-lg text-white font-semibold mb-8">
+            <h1 className=" text-lg text-white font-semibold mb-8">
               A Slack-bot for creating <br />
               and managing prototypes
-            </p>
-            <div className="text-xs text-slate-400 text-justify md:text-left mb-8">
+            </h1>
+            <p className="text-xs text-slate-400 text-justify md:text-left mb-8">
               Building something special? We&#39;r always looking to work with
               partner who wants to help the world bring their ideas to life.
-            </div>
+            </p>
 
             <div className="flex flex-col md:flex-row w-full justify-center items-center md:justify-between">
               <button className="bg-slate-100 w-2/3 md:w-48 text-xs font-semibold text-slate-950 rounded-full flex justify-center items-center py-2 px-1 md:px-3">
